6a71c79b31cafc3ffd0e7dfa6a035bba8fb11f71
David Joseph <davidjosephind@gmail.com>
2023-04-26 03:05:13 +0530

    Add prefix and postfix logic

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
+/**
+ * Get the value from the infix-expression input.
+ *
+ * @returns {string}
+ */
+function getInfixExpression() {
+  return document.getElementById("infix-expression").value || "";
+}
 
+/**
+ * Set the value of the prefix-expression input.
+ *
+ * @param {string} prefix
+ */
+function setPrefixExpression(prefix) {
+  document.getElementById("prefix-expression").value = prefix || "";
+}
+
+/**
+ * Set the value of the postfix-expression input.
+ *
+ * @param {string} postfix
+ */
+function setPostfixExpression(postfix) {
+  document.getElementById("postfix-expression").value = postfix || "";
+}
+
+/**
+ * Convert the infix value to prefix and postfix.
+ */
+function convert() {
+  const infix = getInfixExpression();
+
+  const prefix = infixToPrefix(infix || "");
+  const postfix = infixToPostfix(infix || "");
+
+  setPrefixExpression(prefix);
+  setPostfixExpression(postfix);
+}
